576923c1ff5f26e9d0ffe4cea3d6c2ee29d5383f
Jhonatan Lobo <jhon19lob@gmail.com>
2026-09-18 09:56:28 -0300

    feat(catalog): criar data patch para provisionar atributo selo sustentavel

---
+++ src/app/code/Webjump/Jhonatan/Setup/Patch/Data/AddSeloSustentavelProductAttribute.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace Webjump\Jhonatan\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class AddSeloSustentavelProductAttribute implements DataPatchInterface
+{
+    private ModuleDataSetupInterface $moduleDataSetup;
+    private EavSetupFactory $eavSetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function apply(): void
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'selo_sustentavel',
+            [
+                'type' => 'int',
+                'label' => 'Selo Sustentável',
+                'input' => 'boolean',
+                'source' => Boolean::class,
+                'required' => false,
+                'sort_order' => 100,
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'visible' => true,
+                'user_defined' => true,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => true,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => '',
+                'group' => 'General',
+                'default' => '0'
+            ]
+        );
+    }
+
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
